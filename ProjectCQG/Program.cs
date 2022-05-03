00fcea9046ad5539028e1ecacb25d3ca1f240941
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2022-05-03 14:27:05 +0400

    Added a test array of words

--- ProjectCQG/Program.cs
+++ ProjectCQG/Program.cs
@@ -1,5 +1,11 @@
 ﻿
+using ProjectCQG;
+
 class Program
 {
-    
+    static void Main(string[] args)
+    {
+        var spelling = new Spelling();
+        string[] word = { "hte", "rame", "in", "pain", "fells", "mainy", "oon", "teh", "lain", "was", "hints", "pliant" };
+    }
 }
