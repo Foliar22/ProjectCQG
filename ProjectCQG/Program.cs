ad404efce0b2e57b42639cf97c69732f5c27e26c
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2022-05-03 19:30:50 +0400

    Added properties for paths in Program

--- ProjectCQG/Program.cs
+++ ProjectCQG/Program.cs
@@ -3,6 +3,11 @@ namespace ProjectCQG
 {
     class Program
     {
+        private static string _directory = @"D:\";
+        private static string PathInput { get; } = $"{_directory}Input.txt";
+        private static string PathOutput { get; } = $"{_directory}OutPut.txt";
+        private static string PathDictionary { get; } = $"{_directory}dictionary.txt";
+        private static string PathWrongWords { get; } = $"{_directory}wrongwords.txt";
         static void Main(string[] args)
         {
             var spelling = new Spelling();
