2fc232b0b9456e51f31dfc2ed3ad48452921527a
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2022-05-03 19:51:42 +0400

    Добавил методы для работы с файлами

--- ProjectCQG/Data.cs
+++ ProjectCQG/Data.cs
@@ -12,5 +12,53 @@ namespace ProjectCQG
         {
         }
 
+        private void ReadAndWrite(string path, string dictionaryPath, string wrongWordsPath)
+        {
+            try
+            {
+                var dictionaryLinesList = new List<string>();
+                var wrongWordsList = new List<string>();
+                using (StreamReader sr = new StreamReader(path, Encoding.Default))
+                {
+                    string? line;
+                    while ((line = sr.ReadLine()) != null && line != "===")
+                    {
+                        dictionaryLinesList.Add(line);
+                    }
+                    WriteToFile(dictionaryPath, dictionaryLinesList);
+                    dictionaryLinesList = null;
+                    while ((line = sr.ReadLine()) != null && line != "===")
+                    {
+                        wrongWordsList.Add(line);
+                    }
+                    WriteToFile(wrongWordsPath, wrongWordsList);
+                    wrongWordsList = null;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException + "\n" + ex.Message + "\n" + ex.TargetSite);
+            }
+
+
+        }
+        public void WriteToFile(string path, List<string> dataList)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path, false, Encoding.Default))
+                {
+                    foreach (var item in dataList)
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException + "\n" + ex.Message + "\n" + ex.TargetSite);
+            }
+        }
     }
 }
