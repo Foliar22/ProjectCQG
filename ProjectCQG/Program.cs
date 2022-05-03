07ce977dd7d7c1d4f2b913c9b8f5ea67315e8a3e
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2022-05-03 14:34:18 +0400

    Made a test output

--- ProjectCQG/Program.cs
+++ ProjectCQG/Program.cs
@@ -7,6 +7,16 @@ namespace ProjectCQG
         {
             var spelling = new Spelling();
             string[] word = { "hte", "rame", "in", "pain", "fells", "mainy", "oon", "teh", "lain", "was", "hints", "pliant" };
+            for (int i = 0; i < word.Length; i++)
+            {
+                var correctWordArrey = spelling.Correct(word[i]);
+                if (correctWordArrey.Length > 1)
+                {
+                    string aggregateString = correctWordArrey.Aggregate("{", (first, next) => $"{first} {next}") + " }";
+                    Console.WriteLine($"{word[i]} => {aggregateString}");
+                }
+                Console.WriteLine($"{word[i]} => {correctWordArrey[0]}");
+            }
         }
     }
 
